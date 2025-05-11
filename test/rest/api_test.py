376cf7014ee6250e28a7a53b726762e36202761b
Javier Collado <socche@gmail.com>
2025-05-11 12:09:48 +0200

    Add endpoints multiply y divide and test

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import pytest
 
@@ -24,6 +25,32 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "3", "ERROR ADD"
         )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4", "ERROR MULTIPLY"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1.0", "ERROR DIVIDE"  # 👈 corregido aquí también
+        )
+
+    def test_api_divide_cero(self):
+        url = f"{BASE_URL}/calc/divide/2/0"
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, http.client.NOT_ACCEPTABLE)
 
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
